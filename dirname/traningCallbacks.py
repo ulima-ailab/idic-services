ee653c1964e3d64ee3804fb4b53475573b5ca6e5
Angela Mayhua <amayhuaq@gmail.com>
2023-12-05 02:03:07 -0500

    export data for training of interruptibility

--- dirname/traningCallbacks.py
+++ dirname/traningCallbacks.py
@@ -1,63 +1,70 @@
-import datetime
 import numpy as np
 import pandas as pd
-from firebase_admin import firestore
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 import dirname.managers.firestoreManager as fsm
-import dirname.managers.DataManager as dm
-from dirname.inference_engine.FuzzySystem import FuzzySystem
-from dirname.inference_engine.SVMModel import SVMmodel
-from dirname.inference_engine.enfs.enfs import ENFS
-from dirname.inference_engine.models import *
-
-import os
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv(), override=True)
+import dirname.settings as settings
 
 
 @csrf_exempt
 def export_interruptibility_raw(request):
-    app_data = fsm.db_get_documents_from_collection("Test_AppData")
-    web_data = fsm.db_get_documents_from_collection("Test_WebApp")
-    users = fsm.db_get_documents_from_collection("TestUsers")
+    df_app_data = fsm.db_get_documents_from_collection("Test_AppData").sort_values(by=['id_user', 'delivery_time'], ascending=[True, False], na_position='first')
+    df_web_data = fsm.db_get_documents_from_collection("Test_WebApp").sort_values(by=['id_user', 'timestamp'], ascending=[True, False], na_position='first')
+    df_users = fsm.db_get_documents_from_collection("TestUsers").sort_values(by=['id'], ascending=[True])
 
+    df_app_data = df_app_data.reset_index()
+    next_idx = len(df_app_data)
+    print(df_app_data, next_idx)
+    print(df_web_data)
+    new_indices = []
+    curr_idx = 0
+    MAX_DELAY_SECS = 60
 
-@csrf_exempt
-def start_message_generation(request):
-    user_id = request.POST.get('userId')
-    current_time = request.POST.get('currentTime')
-    end_timestamp = datetime.datetime.strptime(current_time, '%Y-%m-%d %H:%M:%S')
-    start_timestamp = end_timestamp - datetime.timedelta(minutes=int(os.environ.get('MINS_FOR_PERSUASION_INFERENCE')))
+    for index, row in df_app_data.iterrows():
+        print("Evaluating " + str(index))
+        while True:
+            if row["id_user"] == df_web_data.iloc[curr_idx]["id_user"]:
+                # convert timestamps to datetime object
+                start_ts_ = row["delivery_time"] # datetime.fromtimestamp(row["delivery_time"])
+                end_ts = df_web_data.iloc[curr_idx]["timestamp"] # datetime.fromtimestamp(df_web_data.iloc[curr_idx]["timestamp"])
+                # Difference between two timestamps in seconds
+                delta = end_ts - start_ts_
+                if abs(delta.total_seconds()) <= MAX_DELAY_SECS:
+                    print("ADD current index")
+                    new_indices.append(index)
+                    curr_idx += 1
+                    break
+                else:
+                    if delta.total_seconds() > 0:   # It means that the current web register is less than the current row
+                        print("ADD NEW index")
+                        print(next_idx)
+                        new_indices.append(next_idx)
+                        next_idx += 1
+                        curr_idx += 1
+                    else:
+                        break
+            else:
+                if row["id_user"] < df_web_data.iloc[curr_idx]["id_user"]:
+                    break
+                else:
+                    new_indices.append(next_idx)
+                    next_idx += 1
+                    curr_idx += 1
 
-    print("StartTime", start_timestamp)
-    print("EndTime", end_timestamp)
+    while curr_idx < len(df_web_data):
+        new_indices.append(next_idx)
+        next_idx += 1
+        curr_idx += 1
 
-    data = fsm.db_get_emotions(user_id, start_timestamp, end_timestamp)
-    data = dm.preprocess_data(data)
-    result = {"emotion_data": data}
+    df_web_data.index = np.array(new_indices)
+    print(df_web_data)
 
-    model_id = os.environ.get('MODEL_PERSUASION_INFERENCE')
+    df_all = pd.concat([df_app_data, df_web_data], axis=1)
+    #df_all = df_all.drop(columns=['id_user_1', 'index'])
 
-    if model_id == FUZZY_MODEL:
-        model = FuzzySystem()
-    elif model_id == ENFS_MODEL:
-        model = ENFS()
-        model.load_model(ENFS_PATH)
-        tmp = pd.DataFrame([data])
-        data = np.array(tmp[FEATURES_COLS])
-        data = np.array([data])
-    elif model_id == SVM_MODEL:
-        model = SVMmodel()
-        model.load_model(SVM_PATH)
-        tmp = pd.DataFrame([data])
-        data = np.array(tmp[FEATURES_COLS])
-    persuasion_level = model.process_input(data)
-    result[LABEL_COL] = persuasion_level
-
-    result["message"] = generate_message(persuasion_level)
-    print("SERVER: " + persuasion_level)
+    df_all.to_csv(settings.STATIC_URL + "raw_interruptibility_data.csv")
+    df_users.to_csv(settings.STATIC_URL + "raw_users_data.csv")
 
     # Return the collection data as a JSON response
-    return JsonResponse(result, safe=False)
+    return JsonResponse({"msg": "data exported"}, safe=False)
